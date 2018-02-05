ec574a07de1e3f275a137d3a6520bf95acc5ebc9
Grigor Harutyunyan <C:\Users\grigor\AppData\Roaming\The Bat!>
2018-02-05 13:02:21 +0400

    Second Commit

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php 
 	
-	print_r("HELLO WORLD");
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BlackhettDesign</title>
+</head>
+<body>
+	<h1 class="site--title" style="display: block; margin: auto;">BlackhettDesign</h1>
+</body>
+</html>
