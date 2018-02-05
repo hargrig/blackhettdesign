706380aaecf72fd5ffda2283f9994c7047e6ebdc
Grigor Harutyunyan <C:\Users\grigor\AppData\Roaming\The Bat!>
2018-02-05 13:33:24 +0400

    Commit

---
+++ ___index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BlackhettDesign</title>
+	<style>
+		.container {
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<header class="">
+		<div class="container">
+			<h1 class="site--title">BlackhettDesign</h1>
+		</div>
+	</header>
+</body>
+</html>
