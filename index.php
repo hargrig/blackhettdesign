b5782f3afc6c21834f3f4083a8310d13fd3765e9
Grigor Harutyunyan <C:\Users\grigor\AppData\Roaming\The Bat!>
2018-02-05 13:20:57 +0400

    Commit

--- index.php
+++ index.php
@@ -8,6 +8,6 @@
 	<title>BlackhettDesign</title>
 </head>
 <body>
-	<h1 class="site--title" style="display: block; margin: auto;">BlackhettDesign</h1>
+	<h1 class="site--title" style="display: block; margin: 0, auto;">BlackhettDesign</h1>
 </body>
 </html>
