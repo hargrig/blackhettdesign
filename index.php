cc64358fd5a9da906ad4b8d15b0e8ea04b32d506
Grigor Harutyunyan <C:\Users\grigor\AppData\Roaming\The Bat!>
2018-02-06 16:19:57 +0400

    Commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Blackhett</title>
+	<title>BlackhettDesign</title>
 	<meta name="viewport"    content="width=device-width, initial-scale=1">
 	<meta name="description" content="Blackhett Own Page" />
 	<meta name="keywords"    content="blackhett" />
